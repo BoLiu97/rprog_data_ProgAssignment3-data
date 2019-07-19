ffd3bc8c6ffbe6fe5357b331482a592fb47125fe
Bo Liu <daidaidashixiongnmsl@gmail.com>
2019-07-19 12:04:00 +0800

    adding if statement to check if more than one option

--- best.R
+++ best.R
@@ -5,32 +5,35 @@ best <- function(state, outcome) {
   ## rate
   outcome1 = read.csv("outcome-of-care-measures.csv", colClasses = "character")
   ##check if state is a true arg
-  if(outcome1$State &in& state) 
+  if( state %in% outcome1$State) 
     stop(print("invalid outcome"))
   ## also can write as data <- subset(outcome1, State == state)
   data = outcome1[outcome1$State==state,]
-  ##length= nrow(data)
-  
+
   ## check outcome location and true arg
-  if (outcome == "heart attack") colnum <- 11
-  else if (outcome == "heart failure") colnum <-17
-  else if(outcome=="pneumonia") colnum <- 23
-  else stop(print("invalid outcome"))
+  if (outcome == "heart attack") {
+    colnum <- 11}
+  else if (outcome == "heart failure") {
+    colnum <-17}
+  else if(outcome=="pneumonia"){
+    
+    colnum <- 23}
+  else{ 
+    stop(print("invalid outcome"))}
   ## delete NA value
   disease_col <- as.numeric(data[, colnum])
   NA_line <- is.na(disease_col)
-  data_without_na <- data(!NA_line,)
+  data_without_na <- data[!NA_line,]
   
   ##find the smallest value 
-  col_for_sma <- as.numeric(data_without_na(,colnum))
-  locateat < which.min(col_for_sma)
-  hos_smal <- data_without_na()
+  col_for_sma <- as.numeric(data_without_na[,colnum])
+  locateat <- which.min(col_for_sma)
+  hos_smal <- data_without_na[locateat,2]
   
-  fd =data[,c(2,col)]
-  colnames(fd) = c("hospital", "outcome")
-  oi = as.numeric(fd[, eval(2)]) 
-  min1 = min(oi,na.rm = TRUE)
-  result  = fd[, "hospital"][which(oi == min_val)]
-  output  <- result[order(result)]  
-  output
+  ##using sort function here if hospital is more than 1
+  if(length(hos_smal)>1){
+    sort(hos_smal)
+    return(hos_smal[1])}
+  
+  hos_smal
 }
